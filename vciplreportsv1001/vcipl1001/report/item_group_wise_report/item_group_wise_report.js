537af5ec916f105cbaad09546577c75a1a2da67b
saimore1234 <moresai899@gmail.com>
2026-01-12 15:51:14 +0530

    Item Group Report

---
+++ vciplreportsv1001/vcipl1001/report/item_group_wise_report/item_group_wise_report.js
@@ -0,0 +1,41 @@
+frappe.query_reports["Item Group Wise Report"] = {
+    filters: [
+        {
+            fieldname: "company",
+            label: "Company",
+            fieldtype: "Link",
+            options: "Company",
+            default: frappe.defaults.get_user_default("Company"),
+            reqd: 1
+        },
+        {
+            fieldname: "year",
+            label: "Year",
+            fieldtype: "Select",
+            options: ["2023", "2024", "2025"],
+            default: new Date().getFullYear().toString(),
+            reqd: 1
+        },
+        {
+            fieldname: "month",
+            label: "Month",
+            fieldtype: "Select",
+            options: [
+                { label: "January", value: 1 },
+                { label: "February", value: 2 },
+                { label: "March", value: 3 },
+                { label: "April", value: 4 },
+                { label: "May", value: 5 },
+                { label: "June", value: 6 },
+                { label: "July", value: 7 },
+                { label: "August", value: 8 },
+                { label: "September", value: 9 },
+                { label: "October", value: 10 },
+                { label: "November", value: 11 },
+                { label: "December", value: 12 },
+            ],
+            default: new Date().getMonth() + 1,
+            reqd: 1
+        }
+    ]
+};

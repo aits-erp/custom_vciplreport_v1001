09f67ae21e0cb3e5e96d23e3a14535c5fe286d57
saimore1234 <moresai899@gmail.com>
2026-01-15 18:57:49 +0530

    Add

--- vciplreportsv1001/vcipl1001/report/sales_analytics_report/sales_analytics_report.js
+++ vciplreportsv1001/vcipl1001/report/sales_analytics_report/sales_analytics_report.js
@@ -1,5 +1,3 @@
-// Copyright...
-
 frappe.query_reports["Sales Analytics Report"] = {
     filters: [
         {
@@ -8,49 +6,36 @@ frappe.query_reports["Sales Analytics Report"] = {
             fieldtype: "Select",
             options: [
                 "Customer Group",
-                "Customer",
-                "Item Group",
+                "Supplier",
+                "Supplier Group",
                 "Item",
+                "Item Group",
                 "Territory",
-                "Order Type",
                 "Project",
-				
+                "Order Type"
             ],
-            default: "Customer",
-            reqd: 1,
+            default: "Customer Group",
+            reqd: 1
         },
         {
             fieldname: "doc_type",
-            label: __("based_on"),
+            label: __("Document Type"),
             fieldtype: "Select",
-            options: ["Sales Order", "Delivery Note", "Sales Invoice"],
+            options: [
+                "Sales Invoice",
+                "Sales Order",
+                "Delivery Note"
+            ],
             default: "Sales Invoice",
-            reqd: 1,
+            reqd: 1
         },
         {
             fieldname: "value_quantity",
-            label: __("Value Or Qty"),
+            label: __("Value / Quantity"),
             fieldtype: "Select",
-            options: [
-                { value: "Value", label: __("Value") },
-                { value: "Quantity", label: __("Quantity") },
-            ],
+            options: ["Value", "Quantity"],
             default: "Value",
-            reqd: 1,
-        },
-        {
-            fieldname: "from_date",
-            label: __("From Date"),
-            fieldtype: "Date",
-            default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[1],
-            reqd: 1,
-        },
-        {
-            fieldname: "to_date",
-            label: __("To Date"),
-            fieldtype: "Date",
-            default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[2],
-            reqd: 1,
+            reqd: 1
         },
         {
             fieldname: "company",
@@ -58,82 +43,73 @@ frappe.query_reports["Sales Analytics Report"] = {
             fieldtype: "Link",
             options: "Company",
             default: frappe.defaults.get_user_default("Company"),
-            reqd: 1,
+            reqd: 1
         },
         {
             fieldname: "range",
             label: __("Range"),
             fieldtype: "Select",
-            options: [
-                { value: "Weekly", label: __("Weekly") },
-                { value: "Monthly", label: __("Monthly") },
-                { value: "Quarterly", label: __("Quarterly") },
-                { value: "Yearly", label: __("Yearly") },
-            ],
+            options: ["Monthly", "Quarterly", "Yearly"],
             default: "Monthly",
-            reqd: 1,
+            reqd: 1
         },
         {
-            fieldname: "show_aggregate_value_from_subsidiary_companies",
-            label: __("Show Aggregate Value from Subsidiary Companies"),
-            fieldtype: "Check",
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(frappe.datetime.get_today(), -12),
+            reqd: 1
         },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.get_today(),
+            reqd: 1
+        },
+
+        // 🔽 CUSTOMER MASTER–BASED FILTERS
+        {
+            fieldname: "customer_group",
+            label: __("Customer Group"),
+            fieldtype: "Link",
+            options: "Customer Group"
+        },
+        {
+            fieldname: "custom_sub_group",
+            label: __("Sub Group"),
+            fieldtype: "Data"
+        },
+        {
+            fieldname: "custom_company_type",
+            label: __("Company Type"),
+            fieldtype: "Select",
+            options: "\nProprietor\nPartnership\nPrivate Limited\nGovernment"
+        }
     ],
 
-    get_datatable_options(options) {
-        return Object.assign(options, {
-            checkboxColumn: true,
-            events: {
-                onCheckRow: function (data) {
-                    if (!data) return;
+    /**
+     * Formatter:
+     * - indentation only
+     * - no customer code handling needed (Python already fixed)
+     */
+    formatter: function (value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
 
-                    const data_doctype = $(data[2].html)[0]
-                        .attributes.getNamedItem("data-doctype").value;
-                    const tree_type =
-                        frappe.query_report.filters[0].value;
+        if (!data) return value;
 
-                    if (data_doctype != tree_type) return;
+        if (column.fieldname === "entity" && data.indent !== undefined) {
+            value = `<span style="padding-left:${data.indent * 20}px">${value}</span>`;
+        }
 
-                    const row_name = data[2].content;
-                    const raw_data = frappe.query_report.chart.data;
-
-                    const new_datasets = raw_data.datasets;
-                    const element_found = new_datasets.some(
-                        (element, index, array) => {
-                            if (element.name == row_name) {
-                                array.splice(index, 1);
-                                return true;
-                            }
-                            return false;
-                        }
-                    );
-
-                    const slice_at =
-                        { Customer: 4, Item: 5 }[tree_type] || 3;
-
-                    if (!element_found) {
-                        new_datasets.push({
-                            name: row_name,
-                            values: data
-                                .slice(slice_at, data.length - 1)
-                                .map((c) => c.content),
-                        });
-                    }
-
-                    const new_data = {
-                        labels: raw_data.labels,
-                        datasets: new_datasets,
-                    };
-
-                    const new_options = Object.assign(
-                        {},
-                        frappe.query_report.chart_options,
-                        { data: new_data }
-                    );
-                    frappe.query_report.render_chart(new_options);
-                    frappe.query_report.raw_chart_data = new_data;
-                },
-            },
-        });
+        return value;
     },
+
+    /**
+     * On load safety
+     */
+    onload: function (report) {
+        // Enforce correct tree
+        report.set_filter_value("tree_type", "Customer Group");
+    }
 };

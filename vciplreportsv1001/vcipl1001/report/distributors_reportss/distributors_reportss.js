cb1c7e7aedbfcd024f9f455693dada1bf761ce7b
saimore1234 <moresai899@gmail.com>
2026-01-20 13:05:57 +0530

    Add

--- vciplreportsv1001/vcipl1001/report/distributors_reportss/distributors_reportss.js
+++ vciplreportsv1001/vcipl1001/report/distributors_reportss/distributors_reportss.js
@@ -1,23 +1,28 @@
-frappe.query_reports["Distributor Sales Hierarchy"] = {
+frappe.query_reports["Distributors Reportss"] = {
 
     filters: [
         {
             fieldname: "parent_sales_person",
             label: __("Parent Sales Person"),
             fieldtype: "Link",
-            options: "Sales Person"
+            options: "Sales Person",
+            reqd: 1
+        },
+        {
+            fieldname: "custom_territory",
+            label: __("Territory"),
+            fieldtype: "Link",
+            options: "Territory",
+            reqd: 1
         }
     ],
 
     formatter: function (value, row, column, data, default_formatter) {
         value = default_formatter(value, row, column, data);
 
-        if (data && data.indent === 0) {
-            value = `<b>${value}</b>`;
-        }
-        if (data && data.indent === 1) {
-            value = `<i>${value}</i>`;
-        }
+        if (data.indent === 0) value = `<b>${value}</b>`;
+        if (data.indent === 1) value = `<b>${value}</b>`;
+        if (data.indent === 2) value = `<i>${value}</i>`;
 
         return value;
     }
--- vciplreportsv1001/vcipl1001/report/distributors_reportss/distributors_reportss.py
+++ vciplreportsv1001/vcipl1001/report/distributors_reportss/distributors_reportss.py
@@ -4,111 +4,92 @@ from frappe.utils import flt
 
 def execute(filters=None):
     filters = filters or {}
-
-    columns = get_columns()
     data = []
+    columns = get_columns()
 
-    parent_sp = filters.get("parent_sales_person")
+    parent_sales_person = filters.get("parent_sales_person")
+    root_territory = filters.get("custom_territory")
 
-    # --------------------------------------------------
-    # LEVEL 0 : Parent Sales Person (optional)
-    # --------------------------------------------------
-    if parent_sp:
-        data.append({
-            "name": parent_sp,
-            "amount": get_sales_person_amount(parent_sp),
-            "indent": 0
-        })
+    if not parent_sales_person or not root_territory:
+        return columns, data
 
-    # --------------------------------------------------
-    # LEVEL 1 : Geography (Territory root)
-    # --------------------------------------------------
-    geographies = frappe.get_all(
+    # -------------------------------------------------
+    # LEVEL 0 : Parent Sales Person
+    # -------------------------------------------------
+    data.append({
+        "name": parent_sales_person,
+        "amount": get_sales_person_amount(parent_sales_person),
+        "indent": 0
+    })
+
+    # -------------------------------------------------
+    # LEVEL 1 : Territory (West)
+    # -------------------------------------------------
+    data.append({
+        "name": root_territory,
+        "amount": get_territory_amount(root_territory),
+        "indent": 1
+    })
+
+    # -------------------------------------------------
+    # LEVEL 2 : Sub Territories (Mumbai)
+    # -------------------------------------------------
+    sub_territories = frappe.get_all(
         "Territory",
-        filters={"parent_territory": None},
+        filters={"parent_territory": root_territory},
         fields=["name"]
     )
 
-    for geo in geographies:
+    for sub in sub_territories:
         data.append({
-            "name": geo.name,
-            "amount": get_territory_amount(geo.name),
-            "indent": 1 if parent_sp else 0
+            "name": sub.name,
+            "amount": get_territory_amount(sub.name),
+            "indent": 2
         })
 
-        # --------------------------------------------------
-        # LEVEL 2 : Location
-        # --------------------------------------------------
-        locations = frappe.get_all(
-            "Territory",
-            filters={"parent_territory": geo.name},
+        # -------------------------------------------------
+        # LEVEL 3 : Customers (C0047)
+        # -------------------------------------------------
+        customers = frappe.get_all(
+            "Customer",
+            filters={"territory": sub.name},
             fields=["name"]
         )
 
-        for loc in locations:
+        for cust in customers:
             data.append({
-                "name": loc.name,
-                "amount": get_territory_amount(loc.name),
-                "indent": 2 if parent_sp else 1
+                "name": cust.name,
+                "amount": get_customer_amount(cust.name),
+                "indent": 3
             })
 
-            # --------------------------------------------------
-            # LEVEL 3 : Sales Person
-            # --------------------------------------------------
-            sales_persons = frappe.get_all(
-                "Sales Person",
-                filters={"territory": loc.name},
-                fields=["name"]
-            )
+            # -------------------------------------------------
+            # LEVEL 4 : Sales Person (Ashish Masre)
+            # -------------------------------------------------
+            sales_persons = frappe.db.sql("""
+                SELECT DISTINCT st.sales_person
+                FROM `tabSales Team` st
+                WHERE st.parenttype = 'Customer'
+                  AND st.parent = %s
+            """, cust.name, as_dict=True)
 
             for sp in sales_persons:
-                # If parent selected, show only its hierarchy
-                if parent_sp and not is_child_of(sp.name, parent_sp):
-                    continue
-
                 data.append({
-                    "name": sp.name,
-                    "amount": get_sales_person_amount(sp.name),
-                    "indent": 3 if parent_sp else 2
+                    "name": sp.sales_person,
+                    "amount": get_sales_person_amount(sp.sales_person),
+                    "indent": 4
                 })
-
-                # --------------------------------------------------
-                # LEVEL 4 : Customers
-                # --------------------------------------------------
-                customers = frappe.db.sql("""
-                    SELECT DISTINCT c.name
-                    FROM `tabCustomer` c
-                    JOIN `tabSales Team` st ON st.parent = c.name
-                    WHERE st.sales_person = %s
-                """, sp.name, as_dict=True)
-
-                for cust in customers:
-                    data.append({
-                        "name": cust.name,
-                        "amount": get_customer_amount(cust.name),
-                        "indent": 4 if parent_sp else 3
-                    })
 
     return columns, data
 
 
-# --------------------------------------------------
-# HELPERS
-# --------------------------------------------------
-def is_child_of(sales_person, parent):
-    return frappe.db.exists(
-        "Sales Person",
-        {
-            "name": sales_person,
-            "parent_sales_person": parent
-        }
-    )
-
-
+# -------------------------------------------------
+# COLUMNS
+# -------------------------------------------------
 def get_columns():
     return [
         {
-            "label": "Sales Hierarchy",
+            "label": "Distributor Hierarchy",
             "fieldname": "name",
             "fieldtype": "Data",
             "width": 350
@@ -122,6 +103,9 @@ def get_columns():
     ]
 
 
+# -------------------------------------------------
+# AMOUNT HELPERS
+# -------------------------------------------------
 def get_customer_amount(customer):
     return flt(frappe.db.sql("""
         SELECT SUM(base_grand_total)

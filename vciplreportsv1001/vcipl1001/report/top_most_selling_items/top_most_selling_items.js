060b06e9bbef2b9a4c72c20922ab77b84ed91b9e
Sai Dattaram More <168020850+saimore1234@users.noreply.github.com>
2026-02-16 22:34:08 +0530

    From date to date filter

--- vciplreportsv1001/vcipl1001/report/top_most_selling_items/top_most_selling_items.js
+++ vciplreportsv1001/vcipl1001/report/top_most_selling_items/top_most_selling_items.js
@@ -1,7 +1,14 @@
 frappe.query_reports["Top Most Selling Items"] = {
     onload: function (report) {
+        const today = frappe.datetime.get_today();
+        const year = frappe.datetime.str_to_obj(today).getMonth() > 2
+            ? frappe.datetime.str_to_obj(today).getFullYear()
+            : frappe.datetime.str_to_obj(today).getFullYear() - 1;
+
         report.set_filter_value("custom_item_type", "Finished Goods");
         report.set_filter_value("record_limit", 50);
+        report.set_filter_value("from_date", `${year}-04-01`);
+        report.set_filter_value("to_date", `${year + 1}-03-31`);
     },
 
     filters: [
@@ -9,8 +16,19 @@ frappe.query_reports["Top Most Selling Items"] = {
             fieldname: "custom_item_type",
             label: __("Item Type"),
             fieldtype: "Data",
-            default: "Finished Goods",
-            reqd: 0
+            default: "Finished Goods"
+        },
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            reqd: 1
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            reqd: 1
         },
         {
             fieldname: "record_limit",

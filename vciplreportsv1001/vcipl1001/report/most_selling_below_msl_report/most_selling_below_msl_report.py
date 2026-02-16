af127cc388c5fca0510856791d8110b44fd8e024
Sai Dattaram More <168020850+saimore1234@users.noreply.github.com>
2026-02-16 22:16:03 +0530

    MSL reports

--- vciplreportsv1001/vcipl1001/report/most_selling_below_msl_report/most_selling_below_msl_report.py
+++ vciplreportsv1001/vcipl1001/report/most_selling_below_msl_report/most_selling_below_msl_report.py
@@ -1,136 +1,3 @@
-# import frappe
-# from datetime import date
-
-
-# def execute(filters=None):
-#     filters = filters or {}
-
-#     today = date.today()
-#     fy_year = today.year if today.month > 3 else today.year - 1
-
-#     filters["from_date"] = date(fy_year, 4, 1)
-#     filters["to_date"] = date(fy_year + 1, 3, 31)
-
-#     return get_columns(), get_data(filters)
-
-
-# def get_columns():
-#     return [
-#         {
-#             "label": "Item Code",
-#             "fieldname": "item_code",
-#             "fieldtype": "Link",
-#             "options": "Item",
-#             "width": 180
-#         },
-#         {
-#             "label": "Item Name",
-#             "fieldname": "item_name",
-#             "fieldtype": "Data",
-#             "width": 320
-#         },
-#         {
-#             "label": "Total Sales Amount",
-#             "fieldname": "total_amount",
-#             "fieldtype": "Currency",
-#             "width": 150
-#         },
-#         {
-#             "label": "Total Sold Qty",
-#             "fieldname": "total_qty",
-#             "fieldtype": "Float",
-#             "width": 120
-#         },
-#         {
-#             "label": "Total Stock Qty",
-#             "fieldname": "total_stock_qty",
-#             "fieldtype": "Float",
-#             "width": 120
-#         },
-#         {
-#             "label": "Minimum Stock Level",
-#             "fieldname": "min_stock_level",
-#             "fieldtype": "Float",
-#             "width": 150
-#         },
-#         {
-#             "label": "Shortage Qty",
-#             "fieldname": "shortage_qty",
-#             "fieldtype": "Float",
-#             "width": 120
-#         },
-#         {
-#             "label": "Warehouses",
-#             "fieldname": "details",
-#             "fieldtype": "Data",
-#             "width": 120
-#         }
-#     ]
-
-
-# def get_data(filters):
-#     params = {
-#         "docstatus": 1,
-#         "from_date": filters["from_date"],
-#         "to_date": filters["to_date"],
-#         "item_type": filters.get("custom_item_type"),
-#         "item_code": filters.get("item_code")
-#     }
-
-#     sales_query = """
-#         SELECT
-#             sii.item_code,
-#             i.item_name,
-#             COALESCE(i.safety_stock, 0) AS min_stock_level,
-#             SUM(sii.amount) AS total_amount,
-#             SUM(sii.qty) AS total_qty
-#         FROM `tabSales Invoice Item` sii
-#         INNER JOIN `tabSales Invoice` si ON si.name = sii.parent
-#         INNER JOIN `tabItem` i ON i.name = sii.item_code
-#         WHERE
-#             si.docstatus = %(docstatus)s
-#             AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
-#             AND (%(item_type)s IS NULL OR i.custom_item_type = %(item_type)s)
-#             AND (%(item_code)s IS NULL OR sii.item_code = %(item_code)s)
-#         GROUP BY sii.item_code, i.item_name, i.safety_stock
-#         ORDER BY total_amount DESC
-#     """ 
-
-#     rows = frappe.db.sql(sales_query, params, as_dict=True)
-#     if not rows:
-#         return []
-
-#     item_codes = [r.item_code for r in rows]
-
-#     bins = frappe.get_all(
-#         "Bin",
-#         filters={"item_code": ["in", item_codes]},
-#         fields=["item_code", "actual_qty"]
-#     )
-
-#     stock_map = {}
-#     for b in bins:
-#         stock_map[b.item_code] = stock_map.get(b.item_code, 0) + b.actual_qty
-
-#     final_rows = []
-#     for r in rows:
-#         total_stock = stock_map.get(r.item_code, 0)
-#         msl = r.min_stock_level or 0
-
-#         if total_stock <= 0:
-#             continue
-
-#         if total_stock >= msl:
-#             continue
-
-#         r["total_stock_qty"] = total_stock
-#         r["shortage_qty"] = msl - total_stock
-#         r["details"] = "View Warehouses"
-
-#         final_rows.append(r)
-
-#     return final_rows
-
 import frappe
 from datetime import date
 
@@ -166,6 +33,13 @@ def get_columns():
             "fieldname": "item_name",
             "fieldtype": "Data",
             "width": 320
+        },
+        {
+            "label": "Item Group",
+            "fieldname": "item_group",
+            "fieldtype": "Link",
+            "options": "Item Group",
+            "width": 180
         },
         {
             "label": "Total Sales Amount",
@@ -212,13 +86,15 @@ def get_data(filters):
         "from_date": filters["from_date"],
         "to_date": filters["to_date"],
         "item_type": filters.get("custom_item_type"),
-        "item_code": filters.get("item_code")
+        "item_code": filters.get("item_code"),
+        "item_group": filters.get("item_group"),
     }
 
     sales_query = """
         SELECT
             sii.item_code,
             i.item_name,
+            i.item_group,
             COALESCE(i.safety_stock, 0) AS min_stock_level,
             SUM(sii.amount) AS total_amount,
             SUM(sii.qty) AS total_qty
@@ -228,9 +104,11 @@ def get_data(filters):
         WHERE
             si.docstatus = %(docstatus)s
             AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
+            AND i.safety_stock > 0
             AND (%(item_type)s IS NULL OR i.custom_item_type = %(item_type)s)
             AND (%(item_code)s IS NULL OR sii.item_code = %(item_code)s)
-        GROUP BY sii.item_code, i.item_name, i.safety_stock
+            AND (%(item_group)s IS NULL OR i.item_group = %(item_group)s)
+        GROUP BY sii.item_code, i.item_name, i.item_group, i.safety_stock
         ORDER BY total_amount DESC
     """
 

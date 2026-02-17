1ef7362d16f97b91807c6dd6f3730a21038e015b
Sai Dattaram More <168020850+saimore1234@users.noreply.github.com>
2026-02-17 16:38:30 +0530

    changes2

--- vciplreportsv1001/vcipl1001/report/item_category_wise___report/item_category_wise___report.py
+++ vciplreportsv1001/vcipl1001/report/item_category_wise___report/item_category_wise___report.py
@@ -1,3 +1,141 @@
+# import frappe
+# from frappe.utils import flt
+
+
+# def execute(filters=None):
+#     filters = frappe._dict(filters or {})
+
+#     data, customers = get_pivot_data(filters)
+#     columns = get_columns(customers)
+
+#     return columns, data
+
+
+# # ---------------- COLUMNS ----------------
+# def get_columns(customers):
+#     columns = [
+#         {
+#             "label": "Item Group",
+#             "fieldname": "item_group",
+#             "width": 180
+#         },
+#         {
+#             "label": "Main Group",
+#             "fieldname": "custom_main_group",
+#             "width": 180
+#         },
+#         {
+#             "label": "Sub Group",
+#             "fieldname": "custom_sub_group",
+#             "width": 180
+#         }
+#     ]
+
+#     for customer in customers:
+#         columns.append({
+#             "label": customer,
+#             "fieldname": frappe.scrub(customer),
+#             "fieldtype": "Currency",
+#             "width": 150
+#         })
+
+#     return columns
+
+
+# # ---------------- DATA ----------------
+# def get_pivot_data(filters):
+
+#     conditions = ""
+#     values = {
+#         "from_date": filters.from_date,
+#         "to_date": filters.to_date
+#     }
+
+#     if filters.customer:
+#         conditions += " AND si.customer = %(customer)s"
+#         values["customer"] = filters.customer
+
+#     if filters.item_group:
+#         conditions += " AND i.item_group = %(item_group)s"
+#         values["item_group"] = filters.item_group
+
+#     if filters.custom_main_group:
+#         conditions += " AND i.custom_main_group = %(custom_main_group)s"
+#         values["custom_main_group"] = filters.custom_main_group
+
+#     if filters.custom_sub_group:
+#         conditions += " AND i.custom_sub_group = %(custom_sub_group)s"
+#         values["custom_sub_group"] = filters.custom_sub_group
+
+#     if filters.custom_item_type:
+#         conditions += " AND i.custom_item_type = %(custom_item_type)s"
+#         values["custom_item_type"] = filters.custom_item_type
+
+#     if filters.parent_sales_person:
+#         conditions += " AND sp.parent_sales_person = %(parent_sales_person)s"
+#         values["parent_sales_person"] = filters.parent_sales_person
+
+#     raw_data = frappe.db.sql(
+#         f"""
+#         SELECT
+#             i.item_group,
+#             i.custom_main_group,
+#             i.custom_sub_group,
+#             c.customer_name,
+#             SUM(sii.base_net_amount) AS amount
+#         FROM `tabSales Invoice` si
+#         JOIN `tabSales Invoice Item` sii
+#             ON sii.parent = si.name
+#         JOIN `tabItem` i
+#             ON i.name = sii.item_code
+#         JOIN `tabCustomer` c
+#             ON c.name = si.customer
+#         LEFT JOIN `tabSales Team` st
+#             ON st.parent = si.name
+#             AND st.parenttype = 'Sales Invoice'
+#         LEFT JOIN `tabSales Person` sp
+#             ON sp.name = st.sales_person
+#         WHERE si.docstatus = 1
+#           AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
+#           {conditions}
+#         GROUP BY
+#             i.item_group,
+#             i.custom_main_group,
+#             i.custom_sub_group,
+#             c.customer_name
+#         ORDER BY
+#             i.item_group,
+#             i.custom_main_group,
+#             i.custom_sub_group
+#         """,
+#         values,
+#         as_dict=True
+#     )
+
+#     customers = sorted({row.customer_name for row in raw_data})
+#     result = {}
+
+#     for row in raw_data:
+#         item_group = row.item_group or "Undefined"
+#         main_group = row.custom_main_group or "Undefined"
+#         sub_group = row.custom_sub_group or "Undefined"
+#         customer = row.customer_name
+
+#         key = f"{item_group}::{main_group}::{sub_group}"
+
+#         if key not in result:
+#             result[key] = {
+#                 "item_group": item_group,
+#                 "custom_main_group": main_group,
+#                 "custom_sub_group": sub_group
+#             }
+#             for c in customers:
+#                 result[key][frappe.scrub(c)] = 0
+
+#         result[key][frappe.scrub(customer)] += flt(row.amount)
+
+#     return list(result.values()), customers
+
 import frappe
 from frappe.utils import flt
 
@@ -14,21 +152,9 @@ def execute(filters=None):
 # ---------------- COLUMNS ----------------
 def get_columns(customers):
     columns = [
-        {
-            "label": "Item Group",
-            "fieldname": "item_group",
-            "width": 180
-        },
-        {
-            "label": "Main Group",
-            "fieldname": "custom_main_group",
-            "width": 180
-        },
-        {
-            "label": "Sub Group",
-            "fieldname": "custom_sub_group",
-            "width": 180
-        }
+        {"label": "Item Group", "fieldname": "item_group", "width": 180},
+        {"label": "Main Group", "fieldname": "custom_main_group", "width": 180},
+        {"label": "Sub Group", "fieldname": "custom_sub_group", "width": 180}
     ]
 
     for customer in customers:
@@ -84,17 +210,12 @@ def get_pivot_data(filters):
             c.customer_name,
             SUM(sii.base_net_amount) AS amount
         FROM `tabSales Invoice` si
-        JOIN `tabSales Invoice Item` sii
-            ON sii.parent = si.name
-        JOIN `tabItem` i
-            ON i.name = sii.item_code
-        JOIN `tabCustomer` c
-            ON c.name = si.customer
+        JOIN `tabSales Invoice Item` sii ON sii.parent = si.name
+        JOIN `tabItem` i ON i.name = sii.item_code
+        JOIN `tabCustomer` c ON c.name = si.customer
         LEFT JOIN `tabSales Team` st
-            ON st.parent = si.name
-            AND st.parenttype = 'Sales Invoice'
-        LEFT JOIN `tabSales Person` sp
-            ON sp.name = st.sales_person
+            ON st.parent = si.name AND st.parenttype = 'Sales Invoice'
+        LEFT JOIN `tabSales Person` sp ON sp.name = st.sales_person
         WHERE si.docstatus = 1
           AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
           {conditions}
@@ -135,3 +256,33 @@ def get_pivot_data(filters):
         result[key][frappe.scrub(customer)] += flt(row.amount)
 
     return list(result.values()), customers
+
+
+# ---------------- DRILLDOWN METHOD ----------------
+@frappe.whitelist()
+def get_customer_items(customer, item_group, main_group, sub_group):
+
+    data = frappe.db.sql("""
+        SELECT
+            si.name AS invoice,
+            sii.item_name,
+            sii.qty,
+            sii.base_net_amount AS amount
+        FROM `tabSales Invoice` si
+        JOIN `tabSales Invoice Item` sii ON sii.parent = si.name
+        JOIN `tabItem` i ON i.name = sii.item_code
+        JOIN `tabCustomer` c ON c.name = si.customer
+        WHERE si.docstatus = 1
+            AND c.customer_name = %(customer)s
+            AND IFNULL(i.item_group,'Undefined') = %(item_group)s
+            AND IFNULL(i.custom_main_group,'Undefined') = %(main_group)s
+            AND IFNULL(i.custom_sub_group,'Undefined') = %(sub_group)s
+        ORDER BY si.posting_date DESC
+    """, {
+        "customer": customer,
+        "item_group": item_group,
+        "main_group": main_group,
+        "sub_group": sub_group
+    }, as_dict=True)
+
+    return data

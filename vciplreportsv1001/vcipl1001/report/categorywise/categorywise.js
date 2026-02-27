6fc0c6ca8b7c5b1f9ee5508c5a1a6ca3cae09ad6
saimore1234 <moresai881@gmai.com>
2026-02-27 12:31:34 +0530

    your message

--- vciplreportsv1001/vcipl1001/report/categorywise/categorywise.js
+++ vciplreportsv1001/vcipl1001/report/categorywise/categorywise.js
@@ -1,8 +1,33 @@
-// Copyright (c) 2026, AITS and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["CATEGORYWISE"] = {
-	"filters": [
-
-	]
+    "filters": [
+        {
+            "fieldname": "year",
+            "label": "Year",
+            "fieldtype": "Int",
+            "default": new Date().getFullYear(),
+            "reqd": 1
+        },
+        {
+            "fieldname": "main_group",
+            "label": "Main Group",
+            "fieldtype": "Data"
+        },
+        {
+            "fieldname": "head_sales_person",
+            "label": "Head Sales Person",
+            "fieldtype": "Link",
+            "options": "Sales Person"
+        },
+        {
+            "fieldname": "tso",
+            "label": "TSO",
+            "fieldtype": "Data"
+        },
+        {
+            "fieldname": "customer",
+            "label": "Customer",
+            "fieldtype": "Link",
+            "options": "Customer"
+        }
+    ]
 };

ad7f598902d03e4b792f59a6c1390578a85d3d05
Sai Dattaram More <168020850+saimore1234@users.noreply.github.com>
2026-03-02 12:32:50 +0530

    Update stock_report___cumulative.js

--- vciplreportsv1001/vcipl1001/report/stock_report___cumulative/stock_report___cumulative.js
+++ vciplreportsv1001/vcipl1001/report/stock_report___cumulative/stock_report___cumulative.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2025, your company and contributors
+// For license information, please see license.txt
+
 frappe.query_reports["Stock Report - Cumulative"] = {
     filters: [
         {

5dc3cb74a1e2f808533d96bf2d8efc7325008ab7
saimore1234 <moresai881@gmai.com>
2026-03-29 13:17:34 +0530

    update

--- vciplreportsv1001/vcipl1001/report/tso_wise_categorywise/tso_wise_categorywise.js
+++ vciplreportsv1001/vcipl1001/report/tso_wise_categorywise/tso_wise_categorywise.js
@@ -67,25 +67,53 @@
 //         }
 //     ]
 // };
+
 frappe.query_reports["TSO WISE CATEGORYWISE"] = {
     filters: [
+
         {
             fieldname: "from_date",
             label: "From Date",
             fieldtype: "Date",
+            default: frappe.datetime.month_start(),
             reqd: 1
         },
+
         {
             fieldname: "to_date",
             label: "To Date",
             fieldtype: "Date",
+            default: frappe.datetime.month_end(),
             reqd: 1
         },
+
+        {
+            fieldname: "company",
+            label: "Company",
+            fieldtype: "Link",
+            options: "Company"
+        },
+
         {
             fieldname: "sales_person",
-            label: "Sales Person",
+            label: "TSO (Sales Person)",
             fieldtype: "Link",
             options: "Sales Person"
+        },
+
+        {
+            fieldname: "customer",
+            label: "Customer",
+            fieldtype: "Link",
+            options: "Customer"
+        },
+
+        {
+            fieldname: "customer_group",
+            label: "Customer Group",
+            fieldtype: "Link",
+            options: "Customer Group"
         }
+
     ]
 };

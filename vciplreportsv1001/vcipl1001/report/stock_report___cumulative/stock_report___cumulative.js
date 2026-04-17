5a0a479e94a3962148157409632644900617344c
saimore1234 <moresai881@gmai.com>
2026-04-17 21:31:07 +0530

    fresh company code

--- vciplreportsv1001/vcipl1001/report/stock_report___cumulative/stock_report___cumulative.js
+++ vciplreportsv1001/vcipl1001/report/stock_report___cumulative/stock_report___cumulative.js
@@ -1,3 +1,49 @@
+// frappe.query_reports["Stock Report - Cumulative"] = {
+//     filters: [
+//         {
+//             fieldname: "custom_item_type",
+//             label: __("Item Type"),
+//             fieldtype: "Data",
+//             default: "Finished Goods"
+//         },
+//         {
+//             fieldname: "custom_main_group",
+//             label: __("Main Group"),
+//             fieldtype: "Data"
+//         },
+//         {
+//             fieldname: "item_group",
+//             label: __("Item Group"),
+//             fieldtype: "Link",
+//             options: "Item Group"
+//         },
+//         {
+//             fieldname: "from_date",
+//             label: __("From Date"),
+//             fieldtype: "Date",
+//             default: "2025-04-01"
+//         },
+//         {
+//             fieldname: "to_date",
+//             label: __("To Date"),
+//             fieldtype: "Date",
+//             default: frappe.datetime.get_today()
+//         }
+//     ],
+
+//     formatter: function(value, row, column, data, default_formatter) {
+
+//         // 🚀 FORCE ONLY ITEM CODE (NO ITEM NAME)
+//         if (column.fieldname === "item_code") {
+//             return `<a href="/app/item/${value}" target="_blank">${value}</a>`;
+//         }
+
+//         return default_formatter(value, row, column, data);
+//     }
+// };
+
+// //changes
+
 frappe.query_reports["Stock Report - Cumulative"] = {
     filters: [
         {
@@ -32,8 +78,15 @@ frappe.query_reports["Stock Report - Cumulative"] = {
     ],
 
     formatter: function(value, row, column, data, default_formatter) {
+        // FOR ITEM CODE + NAME COLUMN
+        if (column.fieldname === "item_code_name") {
+            if (data && data.item_code && data.item_name) {
+                return `<a href="/app/item/${data.item_code}" target="_blank">${data.item_code} - ${data.item_name}</a>`;
+            }
+            return value;
+        }
 
-        // 🚀 FORCE ONLY ITEM CODE (NO ITEM NAME)
+        // FORCE ONLY ITEM CODE (NO ITEM NAME)
         if (column.fieldname === "item_code") {
             return `<a href="/app/item/${value}" target="_blank">${value}</a>`;
         }
@@ -41,5 +94,3 @@ frappe.query_reports["Stock Report - Cumulative"] = {
         return default_formatter(value, row, column, data);
     }
 };
-
-//changes

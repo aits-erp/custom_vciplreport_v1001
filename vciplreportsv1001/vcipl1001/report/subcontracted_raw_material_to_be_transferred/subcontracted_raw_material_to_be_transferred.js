9fa95ff7a01273cb98c2b8e20e57151964e0ed76
saimore1234 <moresai881@gmai.com>
2026-06-19 10:26:37 +0530

    ODReport11

---
+++ vciplreportsv1001/vcipl1001/report/subcontracted_raw_material_to_be_transferred/subcontracted_raw_material_to_be_transferred.js
@@ -0,0 +1,32 @@
+frappe.query_reports["Subcontracted Raw Material To Be Transferred"] = {
+	filters: [
+		{
+			label: __("Order Type"),
+			fieldname: "order_type",
+			fieldtype: "Select",
+			options: ["Purchase Order", "Subcontracting Order"],
+			default: "Subcontracting Order",
+		},
+		{
+			fieldname: "supplier",
+			label: __("Supplier"),
+			fieldtype: "Link",
+			options: "Supplier",
+			reqd: 1,
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+	],
+};
